ebaf696665876ec05d64491cb72241fcae351f75
Matteo Vaccari <mvaccari@thoughtworks.com>
2024-02-02 10:36:04 +0100

    Now adding new items

--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
@@ -21,12 +22,25 @@ func check(err error) {
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	log.Printf("%s %s", r.Method, r.URL)
-	err := templates.ExecuteTemplate(w, "index.html", todos)
+	err := templates.ExecuteTemplate(w, "index.html", model)
 	check(err)
+}
+
+func newItemHandler(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		log.Printf("Error: %s", err)
+		_, _ = fmt.Fprintf(w, "oh no %s", err)
+		return
+	}
+	log.Printf("%s %s %s", r.Method, r.URL, r.Form)
+	model.Add(r.Form.Get("new-todo"))
+	http.Redirect(w, r, "/", http.StatusFound)
 }
 
 func main() {
 	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/new-todo", newItemHandler)
 
 	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./public/css"))))
 	http.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("./public/js"))))
