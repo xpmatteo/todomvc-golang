fbd2455a092dca0c35189f68debc5e3506ab210f
Matteo Vaccari <vaccari@pobox.com>
2024-02-03 16:54:18 +0100

    Moved all handlers to the web package, now main is neater

--- server.go
+++ server.go
@@ -12,74 +12,16 @@ const port = "8080"
 
 var model = todo.NewList()
 
-func newItemHandler(w http.ResponseWriter, r *http.Request) {
-	err := r.ParseForm()
-	if err != nil {
-		return400(w, err)
-		return
-	}
-	log.Printf("%s %s %s", r.Method, r.URL, r.Form)
-	model.Add(r.Form.Get("new-todo"))
-	http.Redirect(w, r, "/", http.StatusFound)
-}
-
-func toggleHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
-		return
-	}
-
-	err := r.ParseForm()
-	if err != nil {
-		return400(w, err)
-		return
-	}
-	log.Printf("%s %s %s", r.Method, r.URL, r.Form)
-
-	err = model.Toggle(todo.ItemId(r.Form.Get("todoItemId")))
-	if err != nil {
-		return400(w, err)
-		return
-	}
-	http.Redirect(w, r, "/", http.StatusFound)
-}
-
-func editHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
-		return
-	}
-	err := r.ParseForm()
-	if err != nil {
-		return400(w, err)
-		return
-	}
-	log.Printf("%s %s %s", r.Method, r.URL, r.Form)
-
-	itemId := todo.ItemId(r.Form.Get("todoItemId"))
-	title := r.Form.Get("todoItemTitle")
-	err = model.Edit(itemId, title)
-	if err != nil {
-		return400(w, err)
-		return
-	}
-	http.Redirect(w, r, "/", http.StatusFound)
-}
-
-func return400(w http.ResponseWriter, err error) {
-	http.Error(w, err.Error(), http.StatusBadRequest)
-}
-
 func main() {
 	model.Add("foo")
 	model.Add("bar")
 
 	templ := template.Must(template.ParseFiles("templates/index.html"))
 	http.Handle("/", web.Logging(web.MakeIndexHandler(templ, model)))
-	http.HandleFunc("/new-todo", newItemHandler)
-	http.HandleFunc("/toggle", toggleHandler)
-	http.HandleFunc("/edit", editHandler)
-	http.Handle("/destroy", web.MakeDestroyHandler(model))
+	http.Handle("/new-todo", web.Logging(web.MakeNewItemHandler(model)))
+	http.Handle("/toggle", web.Logging(web.MakeToggleHandler(model)))
+	http.Handle("/edit", web.Logging(web.MakeEditHandler(model)))
+	http.Handle("/destroy", web.Logging(web.MakeDestroyHandler(model)))
 
 	http.Handle("/img/", http.StripPrefix("/img/", http.FileServer(http.Dir("./public/img"))))
 	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./public/css"))))
--- web/handlers.go
+++ web/handlers.go
@@ -2,6 +2,7 @@ package web
 
 import (
 	"html/template"
+	"log"
 	"net/http"
 	"xpug.it/todo"
 )
@@ -27,6 +28,65 @@ func MakeIndexHandler(templ *template.Template, model interface{}) http.Handler 
 	})
 }
 
+func MakeNewItemHandler(list *todo.List) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		err := r.ParseForm()
+		if err != nil {
+			badRequest(w, err)
+			return
+		}
+		list.Add(r.Form.Get("new-todo"))
+		http.Redirect(w, r, "/", http.StatusFound)
+	})
+}
+
+func MakeToggleHandler(list *todo.List) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+			return
+		}
+
+		err := r.ParseForm()
+		if err != nil {
+			badRequest(w, err)
+			return
+		}
+		log.Printf("%s %s %s", r.Method, r.URL, r.Form)
+
+		err = list.Toggle(todo.ItemId(r.Form.Get("todoItemId")))
+		if err != nil {
+			badRequest(w, err)
+			return
+		}
+		http.Redirect(w, r, "/", http.StatusFound)
+	})
+}
+
+func MakeEditHandler(list *todo.List) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+			return
+		}
+		err := r.ParseForm()
+		if err != nil {
+			badRequest(w, err)
+			return
+		}
+		log.Printf("%s %s %s", r.Method, r.URL, r.Form)
+
+		itemId := todo.ItemId(r.Form.Get("todoItemId"))
+		title := r.Form.Get("todoItemTitle")
+		err = list.Edit(itemId, title)
+		if err != nil {
+			badRequest(w, err)
+			return
+		}
+		http.Redirect(w, r, "/", http.StatusFound)
+	})
+}
+
 func MakeDestroyHandler(list *todo.List) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		err := r.ParseForm()
@@ -36,4 +96,8 @@ func MakeDestroyHandler(list *todo.List) http.Handler {
 		list.Destroy(todo.ItemId(r.PostForm.Get("todoItemId")))
 		http.Redirect(w, r, "/", http.StatusSeeOther)
 	})
+}
+
+func badRequest(w http.ResponseWriter, err error) {
+	http.Error(w, err.Error(), http.StatusBadRequest)
 }
