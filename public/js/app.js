4695277de0836a7a6323c8f595f4f2c2e7e7605a
Matteo Vaccari <vaccari@pobox.com>
2024-02-04 09:31:57 +0100

    Simplify javascript

--- public/js/app.js
+++ public/js/app.js
@@ -15,18 +15,15 @@ function invokeDestroy(itemId) {
 }
 
 function startEdit(id) {
-	console.log("start edit?")
-	window.location.replace("/?edit=" + id)
+	window.location.replace(window.location.pathname + "?edit=" + id)
+}
+
+function abortEdit() {
+	window.location.replace("/")
 }
 
 function invokeEdit(id) {
 	console.log("invokeEdit", id)
-	const li = document.getElementById("li-" + id);
-	if (!li.classList.contains("editing")) {
-		// the onblur event is triggering because the user
-		// pressed Escape
-		return;
-	}
 	const form = document.getElementById("editForm");
 	const idField = document.getElementById("editFormItemId");
 	const titleField = document.getElementById("editFormItemTitle");
@@ -34,16 +31,6 @@ function invokeEdit(id) {
 	idField.value = id;
 	titleField.value = edit.value
 	return form.submit();
-}
-
-function abortEdit(id) {
-	console.log("abortEdit", id)
-	const li = document.getElementById("li-" + id);
-	li.classList.remove("editing")
-}
-
-function stopEdit(id) {
-	invokeEdit(id)
 }
 
 function handleEditKeyDown(event) {
